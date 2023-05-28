6318b2d99de2182c509e5756dc9d5e259cdaf543
In <in@Ins-MacBook-Pro.local>
2023-05-28 20:11:48 +0700

    first commit

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+  default = "ami-053b0d53c279acc90"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "subnet_id" {
+  default = "subnet-05d4b61a806dd41ab"
+}
+
+variable "keypair" {
+  default = "ec2"
+}
+
+variable "vpc_id" {
+  default = "vpc-026e2b39245010bf2"
+}
